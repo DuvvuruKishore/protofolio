76c6499973b1d61ede19384270b199f488d74f6e
Duvvuru Kishore <duvvurukishore100@gmail.com>
2021-05-03 12:41:55 +0530

    final commit

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -11,6 +11,9 @@ import L_MATERIALUI from "../../assests/skills/material-ui-1.svg";
 import BIGSALE from '../projects/bigsale.png';
 import UNI from '../projects/uni.jpg';
 import MOVIE from '../projects/movie.jpg';
+import OXYGEN from './oxygen.jpg';
+import WHATSAPP from './whatsapp.jpg';
+import TINDER from './tinder.jpg';
 import L_NODE_JS from "../../assests/skills/nodejs.svg";
 import L_EXPRESS from "../../assests/skills/express.svg";
 import L_MONGODB from "../../assests/skills/mongodb.svg";
@@ -28,7 +31,7 @@ import L_JAVASCRIPT from "../../assests/skills/javascript.svg";*/
 
 function project() {
     return (
-    
+    <div>
         <div id="project">
         <h1 className="pt-3 text-center ">PROJECTS</h1>
         <hr/>
@@ -253,6 +256,319 @@ function project() {
               <div className="focus">
               <Accordion>
               <Card style={{ width: '16rem' }} className="card h-200">
+              <Card.Img variant="top" src={OXYGEN} className="img"/>
+              {/*<Card.Body titleTypographyProps={{variant:'h3' }}>Project title:Social Man Kind</Card.Body>*/}
+              
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong>oxygen suppliers with your request
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>By filling the form, request is made</li>
+                          <li>we can view the request as well</li>
+                      
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+    
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CSS3}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="React"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              React
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_MATERIALUI}
+                                alt="Material-UI"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                           
+                              
+                            </span>
+                            Material-Ui
+                          </li>
+                          
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                    
+                    <Card.Body>
+                    <Card.Link href="https://github.com/DuvvuruKishore/oxygen">github</Card.Link>
+                    <Card.Link href="https://wizardly-colden-10389c.netlify.app/">webview</Card.Link>
+                    </Card.Body>
+                    </Card>
+                   
+                    </Accordion>
+                
+              </div>
+
+              </div>
+                  </div>
+                  
+        <div className="down">
+        <div className="d-flex justify-content-between flex-row mt-1 ">
+              <div className="focus">
+            
+              <Accordion>
+              <Card style={{ width: '16rem' }} className="card h-200 ">
+              <Card.Img variant="top" src={WHATSAPP} className="img"/>
+              {/*<Card.Body titleTypographyProps={{variant:'h3' }}>Project title:Social Man Kind</Card.Body>
+              <Card.Body>
+              <h6>project title  :  social man kind</h6>
+               </Card.Body>*/}
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong>a simple chat room like whatsapp
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>we can chat in the chat room</li>
+                          <li>material-ui is used for clean look</li>
+                          
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CSS3}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="React"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              React
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_MATERIALUI}
+                                alt="Material-UI"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                           
+                              
+                            </span>
+                            Material-Ui
+                          </li>
+                          
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_NODE_JS}
+                                alt="NODEJS"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Node Js
+                            </span>
+                          </li>
+                          <li>
+                          <span className="p-2">
+                            <Image
+                              src={L_EXPRESS}
+                              alt="Express"
+                              rounded
+                              className="image-style1 m-1"
+                            ></Image>{" "}
+                            Express
+                          </span>
+                        </li>
+                        <li>
+                        <span className="p-2">
+                          <Image
+                            src={L_MONGODB}
+                            alt="MongoDb"
+                            rounded
+                            className="image-style1 m-1"
+                          ></Image>{" "}
+                          MongoDb
+                        </span>
+                      </li>
+                          
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                    
+                    <Card.Body>
+                    <Card.Link href="https://github.com/DuvvuruKishore/whatsapp">github</Card.Link>
+                    <Card.Link href="https://awesome-lumiere-25be3d.netlify.app/">webview</Card.Link>
+                    </Card.Body>
+                    </Card>
+                   
+                    </Accordion>
+                
+              
+
+              </div>
+
+              <div className="focus">
+              <Accordion>
+              <Card style={{ width: '16rem' }} className="card h-200">
+              <Card.Img variant="top" src={TINDER} className="img"/>
+              {/*<Card.Body titleTypographyProps={{variant:'h3' }}>Project title:Social Man Kind</Card.Body>
+              <Card.Body>
+              <h6>project title  :  social man kind</h6>
+               </Card.Body>*/}
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong>photos swipe option similar to tinder 
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>we get the data from backend</li>
+                          <li>and we can swipe the photos to see other photos</li>
+                        
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                        <li>
+                        <span className="p-2">
+                          <Image
+                            src={L_REACT}
+                            alt="React"
+                            rounded
+                            className="image-style1 m-1"
+                          ></Image>{" "}
+                          React
+                        </span>
+                      </li>
+
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CSS3}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                           <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_MATERIALUI}
+                                alt="Material-UI"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Material-UI
+                            </span>
+                          </li>
+
+                          <li>
+                          <span className="p-2">
+                            <Image
+                              src={L_NODE_JS}
+                              alt="NODEJS"
+                              rounded
+                              className="image-style1 m-1"
+                            ></Image>{" "}
+                            Node Js
+                          </span>
+                        </li>
+                        <li>
+                        <span className="p-2">
+                          <Image
+                            src={L_EXPRESS}
+                            alt="Express"
+                            rounded
+                            className="image-style1 m-1"
+                          ></Image>{" "}
+                          Express
+                        </span>
+                      </li>
+                      <li>
+                      <span className="p-2">
+                        <Image
+                          src={L_MONGODB}
+                          alt="MongoDb"
+                          rounded
+                          className="image-style1 m-1"
+                        ></Image>{" "}
+                        MongoDb
+                      </span>
+                    </li>
+                       
+                          
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                    
+                    <Card.Body>
+                    <Card.Link href="https://github.com/DuvvuruKishore/tinder">github</Card.Link>
+                    <Card.Link href="https://blissful-raman-c366f7.netlify.app/">webview</Card.Link>
+                    </Card.Body>
+                    </Card>
+                   
+                    </Accordion>
+                
+              </div>
+
+              <div className="focus">
+              <Accordion>
+              <Card style={{ width: '16rem' }} className="card h-200">
               <Card.Img variant="top" src={UNI} className="img"/>
               {/*<Card.Body titleTypographyProps={{variant:'h3' }}>Project title:Social Man Kind</Card.Body>*/}
               
@@ -330,6 +646,8 @@ function project() {
 
               </div>
                   </div>   
+     </div>
+                  
         
     )
 }
